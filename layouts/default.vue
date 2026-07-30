60a8f104e58d4d6e9f53a4c731f7f2cc31c1a8ea
Marvi-G <74012483+Marvi-G@users.noreply.github.com>
2026-07-30 15:04:45 +0200

    Added sidebar collapse

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="min-h-screen bg-[var(--color-bg-primary)]">
-    <LayoutSidebar :is-open="sidebarOpen" @close="sidebarOpen = false" />
+    <LayoutSidebar
+      :is-open="sidebarOpen"
+      :collapsed="sidebarCollapsed"
+      @close="sidebarOpen = false"
+      @toggle-collapse="toggleSidebarCollapse"
+    />
 
-    <div class="lg:pl-64 flex flex-col min-h-screen">
+    <div
+      :class="[
+        'flex flex-col min-h-screen transition-all duration-300',
+        sidebarCollapsed ? 'lg:pl-[70px]' : 'lg:pl-64',
+      ]"
+    >
       <LayoutHeader @toggle-sidebar="sidebarOpen = !sidebarOpen" />
 
       <main class="flex-1 p-4 sm:p-6 lg:p-8">
@@ -17,12 +27,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 
 const { initTheme } = useTheme();
 const sidebarOpen = ref(false);
+const sidebarCollapsed = ref(false);
 
+// Load collapsed state from localStorage
 onMounted(() => {
   initTheme();
+  const savedState = localStorage.getItem("sidebarCollapsed");
+  if (savedState === "true") {
+    sidebarCollapsed.value = true;
+  }
 });
+
+function toggleSidebarCollapse() {
+  sidebarCollapsed.value = !sidebarCollapsed.value;
+  localStorage.setItem("sidebarCollapsed", sidebarCollapsed.value.toString());
+}
 </script>
